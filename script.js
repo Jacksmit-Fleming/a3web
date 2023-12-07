3e91a3354d19ec418bf4d4d907a10df44bfd3a13
Rahul Saravanabavan <142858065+rahulsaravanabavan@users.noreply.github.com>
2023-12-07 10:46:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //- Owner : Rahul Saravanabavan
 //- Date  : Nov 28 (template)
 //- From  : https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
 }
-function hideSidebar(){
+function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
@@ -15,7 +15,7 @@ function hideSidebar(){
 //- Date  : Nov 6 (template)
 //- From  : https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
 
-function Modedark(){
+function Modedark() {
 	var element = document.body;
 	element.classList.toggle("dark-mode");
 }
